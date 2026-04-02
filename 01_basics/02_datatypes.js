d66a4785aaaad5eef53d83da3eae686799e7de22
Om salokhe <omsalokhe2020@gmail.com>
2026-04-02 12:19:00 +0000

    added some things

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,3 +3,13 @@ console.log(typeof num)
 let num2=Number(num)
 console.log(typeof num2)
 console.log(num2)
+
+
+let arr=["om","neeraj","anish"]
+let obj={
+    name:"om",
+    age:19,
+}
+let func=function(){
+    console.log("Hello world");
+}
